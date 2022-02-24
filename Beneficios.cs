92e3257ab66b1ee70fe019208356504032b0b845
JaimerVelez <93137798+JaimerVelez@users.noreply.github.com>
2022-02-24 07:41:34 -0500

    g

--- Beneficios.cs
+++ Beneficios.cs
@@ -4,10 +4,5 @@ public class Class1
 {
 	public Class1()
 	{
-	public int Id { get; set; }
-	public string Producto { get; set; }
-	public string Estado { get; set; }
-	public string Plantilla { get; set; }
-
-}
+	}
 }
